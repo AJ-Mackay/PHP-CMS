3ceffda6d258ed94c00ec0bfb351b3fbb0835ff6
Paul Mackay <paul.m.mackay85@gmail.com>
2022-10-07 13:56:14 +0100

    Deleting Categories

--- admin/categories.php
+++ admin/categories.php
@@ -51,14 +51,6 @@
                             </form>
                         </div>
                         <div class="col-xs-6">
-
-                            <?php
-
-                                $query = "SELECT * FROM categories";
-                                $select_categories = mysqli_query($connection, $query);
-
-                            ?>
-
                             <table class="table table-bordered table-hover">
                                 <thead>
                                     <tr>
@@ -70,6 +62,9 @@
 
                                     <?php
 
+                                        $query = "SELECT * FROM categories";
+                                        $select_categories = mysqli_query($connection, $query);
+
                                         while($row = mysqli_fetch_assoc($select_categories)){
                                             $cat_id = $row['cat_id'];
                                             $cat_title = $row['cat_title'];
@@ -77,7 +72,20 @@
                                             echo "<tr>";
                                             echo "<td>{$cat_id}</td>";
                                             echo "<td>{$cat_title}</td>";
+                                            echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
                                             echo "<tr>";
+                                        }
+
+                                    ?>
+
+
+                                    <?php
+
+                                        if(isset($_GET['delete'])) {
+                                            $the_cat_id = $_GET['delete'];
+                                            $query = "DELETE FROM categories WHERE cat_id = {$the_cat_id} ";
+                                            $delete_query = mysqli_query($connection, $query);
+                                            header("Location: categories.php");
                                         }
 
                                     ?>
