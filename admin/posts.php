0437aee560866f5356c894ef8bada11c2f53e295
Paul Mackay <paul.m.mackay85@gmail.com>
2022-10-10 12:13:33 +0100

    Creating an HTML Table in admin to display a list of posts

---
+++ admin/posts.php
@@ -0,0 +1,71 @@
+<?php include "includes/admin_header.php" ?>
+
+<body>
+
+    <div id="wrapper">
+
+        <?php include "includes/admin_navigation.php" ?>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                    <h1 class="page-header">
+                            Welcome to Admin
+                            <small>Author</small>
+                        </h1>
+
+                        <table class="table table-bordered table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Author</th>
+                                    <th>Title</th>
+                                    <th>Category</th>
+                                    <th>Status</th>
+                                    <th>Image</th>
+                                    <th>Tags</th>
+                                    <th>Comments</th>
+                                    <th>Date</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>10</td>
+                                    <td>Edwin Diaz</td>
+                                    <td>Bootstrap Framework</td>
+                                    <td>Bootstrap</td>
+                                    <td>Status</td>
+                                    <td>Image</td>
+                                    <td>Tags</td>
+                                    <td>Comments</td>
+                                    <td>Date</td>
+                                </tr>
+                            </tbody>
+                        </table>
+
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
