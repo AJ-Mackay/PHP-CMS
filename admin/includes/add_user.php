0e7290d0e84f70ac49b68cb0f7abe008dd6aa300
Paul Mackay <paul.m.mackay85@gmail.com>
2022-10-14 14:17:19 +0100

    Adding Users in Admin - Part 2

--- admin/includes/add_user.php
+++ admin/includes/add_user.php
@@ -1,26 +1,26 @@
 <?php 
 
-if(isset($_POST['create_post'])){
-    $post_title = $_POST['title'];
-    $post_author = $_POST['author'];
-    $post_category_id = $_POST['post_category'];
-    $post_status = $_POST['post_status'];
+if(isset($_POST['create_user'])){
+    $user_firstname = $_POST['user_firstname'];
+    $user_lastname = $_POST['user_lastname'];
+    $user_role = $_POST['user_role'];
 
-    $post_image = $_FILES['image']['name'];
-    $post_image_temp = $_FILES['image']['tmp_name'];
+    // $post_image = $_FILES['image']['name'];
+    // $post_image_temp = $_FILES['image']['tmp_name'];
 
-    $post_tags = $_POST['post_tags'];
-    $post_content = $_POST['post_content'];
-    $post_date = date('d-m-y');
+    $username = $_POST['username'];
+    $user_email = $_POST['user_email'];
+    $user_password = $_POST['user_password'];
+    // $post_date = date('d-m-y');
 
-    move_uploaded_file($post_image_temp, "../images/$post_image");
+    // move_uploaded_file($post_image_temp, "../images/$post_image");
 
-    $query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date, post_image, post_content, post_tags, post_status) ";
-    $query .= "VALUES({$post_category_id}, '{$post_title}', '{$post_author}', now(), '{$post_image}', '{$post_content}', '{$post_tags}', '{$post_status}' ) ";
+    // $query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date, post_image, post_content, post_tags, post_status) ";
+    // $query .= "VALUES({$post_category_id}, '{$post_title}', '{$post_author}', now(), '{$post_image}', '{$post_content}', '{$post_tags}', '{$post_status}' ) ";
 
-    $create_post_query = mysqli_query($connection, $query);
+    // $create_post_query = mysqli_query($connection, $query);
 
-    confirm($create_post_query);
+    // confirm($create_post_query);
 }
 
 ?>
@@ -40,19 +40,9 @@ if(isset($_POST['create_post'])){
 
     <div class="form-group">
         <select name="user_role" id="">
-            <?php
-                 $query = "SELECT * FROM users ";
-                 $select_users = mysqli_query($connection, $query);
-
-                 confirm($select_users);
-
-                 while($row = mysqli_fetch_assoc($select_users)){
-                     $user_id = $row['user_id'];
-                     $user_role = $row['user_role'];
-
-                     echo "<option value='{$user_id}'>{$user_role}</option>";
-                 }
-            ?>
+            <option value="subscriber">Select Role</option>
+            <option value="admin">Admin</option>
+            <option value="subscriber">Subscriber</option>
         </select>
     </div>
 
