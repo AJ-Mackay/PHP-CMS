230216eb8970b37035c84e535f251c7af0bac33c
Paul Mackay <paul.m.mackay85@gmail.com>
2022-10-14 14:27:44 +0100

    Adding Users in Admin - Part 3

--- admin/includes/add_user.php
+++ admin/includes/add_user.php
@@ -1,6 +1,7 @@
 <?php 
 
 if(isset($_POST['create_user'])){
+
     $user_firstname = $_POST['user_firstname'];
     $user_lastname = $_POST['user_lastname'];
     $user_role = $_POST['user_role'];
@@ -15,12 +16,12 @@ if(isset($_POST['create_user'])){
 
     // move_uploaded_file($post_image_temp, "../images/$post_image");
 
-    // $query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date, post_image, post_content, post_tags, post_status) ";
-    // $query .= "VALUES({$post_category_id}, '{$post_title}', '{$post_author}', now(), '{$post_image}', '{$post_content}', '{$post_tags}', '{$post_status}' ) ";
+    $query = "INSERT INTO users(user_firstname, user_lastname, user_role, username, user_email, user_password) ";
+    $query .= "VALUES('{$user_firstname}', '{$user_lastname}', '{$user_role}', '{$username}', '{$user_email}', '{$user_password}' ) ";
 
-    // $create_post_query = mysqli_query($connection, $query);
+    $create_user_query = mysqli_query($connection, $query);
 
-    // confirm($create_post_query);
+    confirm($create_user_query);
 }
 
 ?>
