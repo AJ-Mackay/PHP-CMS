0c3cb0364707416059d6bc5f6d4a1b8f8dfdcf54
Paul Mackay <paul.m.mackay85@gmail.com>
2022-10-14 15:03:35 +0100

    Displaying User Values in the Edit Page

--- admin/includes/edit_user.php
+++ admin/includes/edit_user.php
@@ -1,6 +1,24 @@
 <?php 
 
-if(isset($_POST['create_user'])){
+if(isset($_GET['edit_user'])){
+    $the_user_id = $_GET['edit_user'];
+
+    $query = "SELECT * FROM users WHERE user_id = $the_user_id";
+        $select_users_query = mysqli_query($connection, $query);
+    
+        while($row = mysqli_fetch_assoc($select_users_query)){
+            $user_id = $row['user_id'];
+            $username = $row['username'];
+            $user_password = $row['user_password'];
+            $user_firstname = $row['user_firstname'];
+            $user_lastname = $row['user_lastname'];
+            $user_email = $row['user_email'];
+            $user_image = $row['user_image'];
+            $user_role = $row['user_role'];
+        }
+}
+
+if(isset($_POST['edit_user'])){
 
     $user_firstname = $_POST['user_firstname'];
     $user_lastname = $_POST['user_lastname'];
@@ -31,12 +49,12 @@ if(isset($_POST['create_user'])){
     
 <div class="form-group">
         <label for="author">First Name</label>
-        <input type="text" class="form-control" name="user_firstname">
+        <input type="text" value="<?php echo $user_firstname; ?>" class="form-control" name="user_firstname">
     </div>
 
     <div class="form-group">
         <label for="post_status">Last Name</label>
-        <input type="text" class="form-control" name="user_lastname">
+        <input type="text" value="<?php echo $user_lastname; ?>" class="form-control" name="user_lastname">
     </div>
 
     <div class="form-group">
@@ -56,21 +74,21 @@ if(isset($_POST['create_user'])){
 
     <div class="form-group">
         <label for="post_tags">Username</label>
-        <input type="text" class="form-control" name="username">
+        <input type="text" value="<?php echo $username; ?>" class="form-control" name="username">
     </div>
 
     <div class="form-group">
         <label for="post_content">Email</label>
-        <input type="email" class="form-control" name="user_email">
+        <input type="email" value="<?php echo $user_email; ?>"class="form-control" name="user_email">
     </div>
 
     <div class="form-group">
         <label for="post_content">Password</label>
-        <input type="password" class="form-control" name="user_password">
+        <input type="password" value="<?php echo $user_password; ?>" class="form-control" name="user_password">
     </div>
 
     <div class="form-group">
-        <input class="btn btn-primary" type="submit" name="create_user" value="Add User">
+        <input class="btn btn-primary" type="submit" name="edit_user" value="Edit User">
     </div>
 
 </form>
