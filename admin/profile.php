6abb84b54516e681723a5babed8cf1ebd66bff52
Paul Mackay <paul.m.mackay85@gmail.com>
2022-10-17 15:30:46 +0100

    Creating the Profile page

---
+++ admin/profile.php
@@ -0,0 +1,95 @@
+<?php include "includes/admin_header.php" ?>
+
+<body>
+
+    <div id="wrapper">
+
+        <?php include "includes/admin_navigation.php" ?>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                    <h1 class="page-header">
+                            Welcome to Admin
+                            <small>Author</small>
+                        </h1>
+
+                        <form action="" method="post" enctype="multipart/form-data">
+    
+<div class="form-group">
+        <label for="author">First Name</label>
+        <input type="text" value="<?php echo $user_firstname; ?>" class="form-control" name="user_firstname">
+    </div>
+
+    <div class="form-group">
+        <label for="post_status">Last Name</label>
+        <input type="text" value="<?php echo $user_lastname; ?>" class="form-control" name="user_lastname">
+    </div>
+
+    <div class="form-group">
+        <select name="user_role" id="">
+            <option value="<?php echo $user_role; ?>"><?php echo $user_role; ?></option>
+            <?php
+                if($user_role == 'admin')  {
+                    echo "<option value='subscriber'>subscriber</option>";
+                } else {
+                    echo "<option value='admin'>admin</option>";
+                }
+            ?> 
+        </select>
+    </div>
+
+
+
+    <!-- <div class="form-group">
+        <label for="post_image">User Image</label>
+        <input type="file" name="image">
+    </div> -->
+
+    <div class="form-group">
+        <label for="post_tags">Username</label>
+        <input type="text" value="<?php echo $username; ?>" class="form-control" name="username">
+    </div>
+
+    <div class="form-group">
+        <label for="post_content">Email</label>
+        <input type="email" value="<?php echo $user_email; ?>"class="form-control" name="user_email">
+    </div>
+
+    <div class="form-group">
+        <label for="post_content">Password</label>
+        <input type="password" value="<?php echo $user_password; ?>" class="form-control" name="user_password">
+    </div>
+
+    <div class="form-group">
+        <input class="btn btn-primary" type="submit" name="edit_user" value="Edit User">
+    </div>
+
+</form>
+
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
