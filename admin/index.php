609fffe056bddf7b478bc0cb2063f61674851409
Paul Mackay <paul.m.mackay85@gmail.com>
2022-10-19 13:56:51 +0100

    Adding the WYSIWYG Editor

--- admin/index.php
+++ admin/index.php
@@ -206,6 +206,9 @@
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
 
+    <script src="js/summernote.js"></script>
+
+    <script src="js/scripts.js"></script>
 </body>
 
 </html>
