e19d18e07a6692c07514abacc423ffd73cd55adf
Paul Mackay <paul.m.mackay85@gmail.com>
2022-10-19 15:59:26 +0100

    Adding Bulk Options to Posts - Part 4

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -3,6 +3,13 @@
 if(isset($_POST['checkBoxArray'])){
     foreach($_POST['checkBoxArray'] as $checkBoxValue){
         $bulk_options = $_POST['bulk_options'];
+        switch($bulk_options) {
+            case 'published':
+                $query = "UPDATE posts SET post_status = '{$bulk_options}' WHERE post_id = {$checkBoxValue} ";
+                $update_to_published = mysqli_query($connection, $query);
+                confirm($update_to_published);
+                break;
+        }
     }
 }
 
@@ -13,9 +20,9 @@ if(isset($_POST['checkBoxArray'])){
         <div id="bulkOptionsContainer" class="col-xs-4">
             <select class="form-control" name="bulk_options" id="">
                 <option value="">Select Options</option>
-                <option value="">Publish</option>
-                <option value="">Draft</option>
-                <option value="">Delete</option>
+                <option value="published">Publish</option>
+                <option value="draft">Draft</option>
+                <option value="delete">Delete</option>
             </select>
         </div>
         <div class="col-xs-4">
