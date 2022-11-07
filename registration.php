149d9fb279847a3b0eb518c253471b7a62f456c9
Paul Mackay <paul.m.mackay85@gmail.com>
2022-11-07 14:48:20 +0000

    Encrypting User Passwords

--- registration.php
+++ registration.php
@@ -22,6 +22,7 @@
     
         $row = mysqli_fetch_array($select_randsalt_query);
         $salt = $row['randSalt'];
+        $password = crypt($password, $salt);
     
         $query = "INSERT INTO users (username, user_email, user_password, user_role) ";
         $query .= "VALUES('{$username}', '{$email}', '{$password}', 'subscriber' )";
