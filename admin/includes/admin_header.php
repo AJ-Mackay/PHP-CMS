efd367f66680059274499d7dcb573bc989babcf9
Paul Mackay <paul.m.mackay85@gmail.com>
2022-11-16 12:23:20 +0000

    Adding a Loader to the CMS Admin

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -36,6 +36,8 @@ if(!isset($_SESSION['user_role'])) {
 
     <link href="css/summernote.css" rel="stylesheet">
 
+    <link href="css/loader.css" rel="stylesheet">
+
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
