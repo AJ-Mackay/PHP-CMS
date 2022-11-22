cc83208428b4e22f65fcc5d796e4a179c8b81f49
Paul Mackay <paul.m.mackay85@gmail.com>
2022-11-22 11:11:27 +0000

    Add Page

--- admin/includes/add_posts.php
+++ admin/includes/add_posts.php
@@ -38,6 +38,7 @@ if(isset($_POST['create_post'])){
     </div>
 
     <div class="form-group">
+        <label for="post_category">Category</label>
         <select name="post_category" id="">
             <?php
                  $query = "SELECT * FROM categories ";
@@ -55,10 +56,30 @@ if(isset($_POST['create_post'])){
         </select>
     </div>
 
-    <div class="form-group">
+    <!-- <div class="form-group">
         <label for="author">Post Author</label>
         <input type="text" class="form-control" name="author">
+    </div> -->
+
+    <div class="form-group">
+        <label for="users">Users</label>
+        <select name="users" id="">
+            <?php
+                 $query = "SELECT * FROM users ";
+                 $select_users = mysqli_query($connection, $query);
+
+                 confirm($select_users);
+
+                 while($row = mysqli_fetch_assoc($select_users)){
+                     $user_id = $row['user_id'];
+                     $username = $row['username'];
+
+                     echo "<option value='{$user_id}'>{$username}</option>";
+                 }
+            ?>
+        </select>
     </div>
+
 
     <div class="form-group">
         <select name="post_status" id="">
