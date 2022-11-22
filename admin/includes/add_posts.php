a3952ecc16d30dd6c7c7f315ad929ef8b3ada3fa
Paul Mackay <paul.m.mackay85@gmail.com>
2022-11-22 11:29:38 +0000

    Insertion

--- admin/includes/add_posts.php
+++ admin/includes/add_posts.php
@@ -2,7 +2,7 @@
 
 if(isset($_POST['create_post'])){
     $post_title = $_POST['title'];
-    $post_author = $_POST['author'];
+    $post_user = $_POST['post_user'];
     $post_category_id = $_POST['post_category'];
     $post_status = $_POST['post_status'];
 
@@ -15,8 +15,8 @@ if(isset($_POST['create_post'])){
 
     move_uploaded_file($post_image_temp, "../images/$post_image");
 
-    $query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date, post_image, post_content, post_tags, post_status) ";
-    $query .= "VALUES({$post_category_id}, '{$post_title}', '{$post_author}', now(), '{$post_image}', '{$post_content}', '{$post_tags}', '{$post_status}' ) ";
+    $query = "INSERT INTO posts(post_category_id, post_title, post_user, post_date, post_image, post_content, post_tags, post_status) ";
+    $query .= "VALUES({$post_category_id}, '{$post_title}', '{$post_user}', now(), '{$post_image}', '{$post_content}', '{$post_tags}', '{$post_status}' ) ";
 
     $create_post_query = mysqli_query($connection, $query);
 
@@ -63,7 +63,7 @@ if(isset($_POST['create_post'])){
 
     <div class="form-group">
         <label for="users">Users</label>
-        <select name="users" id="">
+        <select name="post_user" id="">
             <?php
                  $query = "SELECT * FROM users ";
                  $select_users = mysqli_query($connection, $query);
