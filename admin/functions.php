444ec15772c4126191155c7bb9e2e9127bd7faaf
Paul Mackay <paul.m.mackay85@gmail.com>
2022-11-23 13:18:48 +0000

    Before Going Online

--- admin/functions.php
+++ admin/functions.php
@@ -1,5 +1,10 @@
 <?php
 
+function escape($string){
+    global $connection;
+    return mysqli_real_escape_string($connection, trim($string));
+}
+
 function redirect($location){
     header("Location:" . $location);
     exit;
