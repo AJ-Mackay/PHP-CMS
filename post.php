de1dab2581a609bad6485058144898b6ee850a9b
Paul Mackay <paul.m.mackay85@gmail.com>
2022-11-24 12:22:07 +0000

    Don't show drafts to visitors

--- post.php
+++ post.php
@@ -23,9 +23,20 @@
                 if(!$send_query) {
                     die("Query Failed");
                 }
+
+                if(isset($_SESSION['user_role']) && $_SESSION['user_role'] == 'admin'){
+                    $query = "SELECT * FROM posts WHERE post_id = $the_post_id ";
+                } else {
+                    $query = "SELECT * FROM posts WHERE post_id = $the_post_id AND post_status = 'published' ";
+                }
             
-            $query = "SELECT * FROM posts WHERE post_id = $the_post_id ";
+            
             $select_all_posts_query = mysqli_query($connection, $query);
+
+            if(mysqli_num_rows($select_all_posts_query) < 1) {
+                echo "<h1 class='text-center'>No posts available</h1>";
+            } else {
+
             while($row = mysqli_fetch_assoc($select_all_posts_query)) {
                 $post_title = $row['post_title'];
                 $post_author = $row['post_author'];
@@ -36,8 +47,7 @@
             ?>
 
                 <h1 class="page-header">
-                    Page Heading
-                    <small>Secondary Text</small>
+                    Posts
                 </h1>
 
                 <!-- First Blog Post -->
@@ -55,7 +65,7 @@
 
                 <hr>
 
-            <?php } } else {
+            <?php } } } else {
                 header("Location: index.php");
             } ?>   
 
