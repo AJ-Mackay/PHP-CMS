1ed826dae9af366f276bc905d11230d015d3d3d6
Paul Mackay <paul.m.mackay85@gmail.com>
2022-11-24 12:54:18 +0000

    Login form visibility / End Module 34

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -18,6 +18,11 @@
 
 
                 <div class="well">
+
+                <?php if(isset($_SESSION['user_role'])): ?>
+                    <h4>Logged in as: <?php echo $_SESSION['username']; ?></h4>
+                    <a href="includes/logout.php" class="btn btn-primary">Logout</a>
+                <?php else: ?>
                     <h4>Login</h4>
                     <form action="includes/login.php" method="post">
                     <div class="form-group">
@@ -30,6 +35,9 @@
                         </span> 
                     </div>
                     </form>
+                <?php endif; ?>
+
+                    
                     <!-- /.input-group -->
                 </div>
 
