602f8026d4512ea7888df06a1bf5aa740db779f2
Paul Mackay <paul.m.mackay85@gmail.com>
2022-11-24 10:42:37 +0000

    No Posts feature home page

--- index.php
+++ index.php
@@ -13,7 +13,38 @@
             <div class="col-md-8">
 
             <?php
-            
+
+$per_page = 2;
+
+if(isset($_GET['page'])) {
+    $page = $_GET['page'];
+} else {
+    $page = "";
+}
+
+if($page == "" || $page == 1) {
+    $page_1 = 0;
+} else {
+    $page_1 = ($page * $per_page) - $per_page;
+}
+
+$post_query_count = "SELECT * FROM posts WHERE post_status = 'published'";
+$find_count = mysqli_query($connection, $post_query_count);
+$count = mysqli_num_rows($find_count);
+
+if($count < 1) {
+    echo "NO POSTS";
+} else {
+
+$count = ceil($count / $per_page);
+
+$query = "SELECT * FROM posts LIMIT $page_1, $per_page";
+$select_all_posts = mysqli_query($connection, $query);
+$post_count = mysqli_num_rows($select_all_posts);
+echo "<div class='huge'>{$post_count}</div>";
+      
+}
+
             $query = "SELECT * FROM posts ";
             $select_all_posts_query = mysqli_query($connection, $query);
             while($row = mysqli_fetch_assoc($select_all_posts_query)) {
@@ -24,10 +55,6 @@
                 $post_image = $row['post_image'];
                 $post_content = substr($row['post_content'], 0, 100);
                 $post_status = $row['post_status'];
-
-                if($post_status != 'published'){
-                    // echo "<h1 class='text-center'>No Posts to show, Sorry.</h1>";
-                } else {
 
             ?>
 
@@ -54,7 +81,7 @@
 
                 <hr>
 
-            <?php } } ?>   
+            <?php } ?>   
 
               
 
