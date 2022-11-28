e91dbb4dbbd2928ef0052d283f60f7a82e479ad6
Paul Mackay <paul.m.mackay85@gmail.com>
2022-11-28 14:46:47 +0000

    Joining Tables - Part 2

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -90,7 +90,7 @@ if(isset($_POST['checkBoxArray'])){
             <?php
                                 
                 // $query = "SELECT * FROM posts ORDER BY post_id DESC";
-                $query = "SELECT posts.post_id, posts.post_author, posts.post_user, posts.post_title, posts.post_category_id, posts.post_status, posts.post_image, posts.post_tags, posts.post_comment_count, posts.post_date, posts.post_views_count, categories.cat_id, categories.cat_title FROM posts LEFT JOIN categories ON posts.post_category_id = categories.cat_id";
+                $query = "SELECT posts.post_id, posts.post_author, posts.post_user, posts.post_title, posts.post_category_id, posts.post_status, posts.post_image, posts.post_tags, posts.post_comment_count, posts.post_date, posts.post_views_count, categories.cat_id, categories.cat_title FROM posts LEFT JOIN categories ON posts.post_category_id = categories.cat_id ORDER BY posts.post_id DESC ";
 
                 $select_posts = mysqli_query($connection, $query);
             
@@ -106,6 +106,8 @@ if(isset($_POST['checkBoxArray'])){
                     $post_comment_count = $row['post_comment_count'];
                     $post_date = $row['post_date'];
                     $post_views_count = $row['post_views_count'];
+                    $category_title = $row['cat_title'];
+                    $category_id = $row['cat_id'];
 
                     echo "<tr>";
                     ?>
@@ -124,17 +126,17 @@ if(isset($_POST['checkBoxArray'])){
 
                     echo "<td>{$post_title}</td>";
 
-                    $query = "SELECT * FROM categories WHERE cat_id = {$post_category_id} ";
-                        $select_categories_id = mysqli_query($connection, $query);
+                    // $query = "SELECT * FROM categories WHERE cat_id = {$post_category_id} ";
+                    //     $select_categories_id = mysqli_query($connection, $query);
 
-                        while($row = mysqli_fetch_assoc($select_categories_id)){
-                            $cat_id = $row['cat_id'];
-                            $cat_title = $row['cat_title'];
+                    //     while($row = mysqli_fetch_assoc($select_categories_id)){
+                    //         $cat_id = $row['cat_id'];
+                    //         $cat_title = $row['cat_title'];
                         
 
-                        echo "<td>{$cat_title}</td>";
+                        echo "<td>{$category_title}</td>";
                         
-                    }
+                    // }
                                                     
                     echo "<td>{$post_status}</td>";
                     echo "<td><img src='../images/$post_image' width='100' alt='images'></td>";
