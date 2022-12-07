e5b19a73c073e3007544e2b1e2208727e86883b7
Paul Mackay <paul.m.mackay85@gmail.com>
2022-12-07 13:28:23 +0000

    Duplicate username function

--- admin/functions.php
+++ admin/functions.php
@@ -123,7 +123,7 @@ function checkUserRole($table, $column, $role){
     return mysqli_num_rows($result);
 }
 
-function is_admin($username = ''){
+function is_admin($username){
     global $connection;
     $query = "SELECT user_role FROM users WHERE username = '$username' ";
     $result = mysqli_query($connection, $query);
@@ -132,6 +132,19 @@ function is_admin($username = ''){
     $row = mysqli_fetch_array($result);
 
     if($row['user_role'] == 'admin'){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function username_exists($username){
+    global $connection;
+    $query = "SELECT username FROM users WHERE username = '$username' ";
+    $result = mysqli_query($connection, $query);
+    confirm($result);
+
+    if(mysqli_num_rows($result) > 0){
         return true;
     } else {
         return false;
