75fd2f1402af1af4fb07461271af1bbf224fb26c
Paul Mackay <paul.m.mackay85@gmail.com>
2023-01-02 16:03:54 +0000

    Updating database with token values / End Module 39

--- forgot.php
+++ forgot.php
@@ -3,7 +3,7 @@
 
 <?php
 
-if(!ifItIsMethod('get') || !$_GET['forgot']){
+if(!ifItIsMethod('get') && !isset($_GET['forgot'])){
     redirect('index.php');
 }
 
@@ -13,7 +13,14 @@ if(ifItIsMethod('post')){
         $length = 50;
         $token = bin2hex(openssl_random_pseudo_bytes($length));
 
-        
+        if(email_exists($email)){
+            if($stmt = mysqli_prepare($connection, "UPDATE users SET token='{$token}' WHERE user_email=?")){
+                mysqli_stmt_bind_param($stmt, "s", $email);
+                mysqli_stmt_execute($stmt);
+                mysqli_stmt_close($stmt);
+            }
+           
+        }
     }
 }
 
