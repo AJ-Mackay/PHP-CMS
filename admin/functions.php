3be67ed26fb2e7f17c22e87cd480ab16b8c86c25
Paul Mackay <paul.m.mackay85@gmail.com>
2023-01-23 13:16:18 +0000

    Creating a helper function to get the logged in user id - Part 2

--- admin/functions.php
+++ admin/functions.php
@@ -47,7 +47,12 @@ function isLoggedIn(){
 function loggedInUserId(){
     if(isLoggedIn()){
         $result = query("SELECT * FROM users WHERE  username='" . $_SESSION['username'] . "'");
+        $user = mysqli_fetch_array($result);
+        if(mysqli_num_rows($result) >= 1){
+            return $user['user_id'];
+        }
     }
+    return false;
 }
 
 function checkIfUserIsLoggedInAndRedirect($redirectLocation=null){
